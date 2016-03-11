596094024de5c7075a8ae6942f479acf99af76f7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-03-11 12:06:54 +0100

    Fix file headers and add LICENSE

--- src/ContaoCommunityAlliance/Contao/Bindings/Events/Calendar/GetCalendarEventEvent.php
+++ src/ContaoCommunityAlliance/Contao/Bindings/Events/Calendar/GetCalendarEventEvent.php
@@ -1,15 +1,21 @@
 <?php
+
 /**
- * The Contao Community Alliance events-contao-bindings library allows easy use of various Contao classes.
+ * This file is part of contao-community-alliance/events-contao-bindings
  *
- * PHP version 5
+ * (c) 2014-2016 The Contao Community Alliance
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    ContaoCommunityAlliance\Contao\Bindings\Events
- * @subpackage Controller
+ * @subpackage Calendar
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan.lins@bit3.de>
- * @copyright  The Contao Community Alliance
- * @license    LGPL.
+ * @copyright  2014 The Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/events-contao-bindings/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
