111aec295fb74b82e96395991ba9b5b92307a245
Sven Baumann <baumann.sv@gmail.com>
2017-06-30 22:56:30 +0200

    Register subscriber in the service container

---
+++ src/DependencyInjection/CcaEventsContaoBindingsExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/events-contao-bindings
+ *
+ * (c) 2014-2017 The Contao Community Alliance
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    ContaoCommunityAlliance\Contao\Bindings
+ * @subpackage System
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2017 The Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/events-contao-bindings/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\Contao\Bindings\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * This is the class that loads and manages the bundle configuration.
+ */
+class CcaEventsContaoBindingsExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
