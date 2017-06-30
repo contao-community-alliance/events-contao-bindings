6774a079f24a0b79ae71823b1ac64ff8e6a369d2
Sven Baumann <baumann.sv@gmail.com>
2017-06-30 12:28:21 +0200

    Support contao managed edition

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of contao-community-alliance/events-contao-bindings
+ *
+ * (c) 2014-2017 The Contao Community Alliance
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    ContaoCommunityAlliance\Contao\Bindings
+ * @subpackage System
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2017 The Contao Community Alliance.
+ * @license    https://github.com/contao-community-alliance/events-contao-bindings/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\Contao\Bindings\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerBundle\ContaoManagerBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoCommunityAlliance\Contao\EventDispatcher\CcaEventDispatcherBundle;
+
+/**
+ * Plugin for the Contao Manager.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CcaEventDispatcherBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                        ContaoManagerBundle::class,
+                        CcaEventDispatcherBundle::class
+                    ]
+                )
+        ];
+    }
+}
